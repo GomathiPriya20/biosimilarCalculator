8ce4d71f0f5d05593d8b4d001c7da9d211ef21f2
GomathiPriya20 <78908660+GomathiPriya20@users.noreply.github.com>
2025-02-21 18:46:54 +0530

    Pushing latest changes as of Feb 21 2025

--- BiosimilarCalculator/src/main/java/base/PreAndPost.java
+++ BiosimilarCalculator/src/main/java/base/PreAndPost.java
@@ -4,11 +4,9 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
-import org.json.JSONObject;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
@@ -24,96 +22,91 @@ import com.aventstack.extentreports.reporter.configuration.Theme;
 
 import events.WebDriverEvents;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import utils.APIUtils;
 import utils.DataInputProvider;
-import utils.SessionManager;
 
- 
-public class PreAndPost extends WebDriverEvents
-{
-    public static ExtentHtmlReporter htmlReporter;
-    public static ExtentReports extent;
-    public static ExtentTest test;
+public class PreAndPost extends WebDriverEvents {
+	public static ExtentHtmlReporter htmlReporter;
+	public static ExtentReports extent;
+	public static ExtentTest test;
 	public static Properties properties = new Properties();
- 
-    private static String reportFileName = "Test-Automation-Report";
-    private static String fileSeperator = System.getProperty("file.separator");
-    private static String reportFilepath = System.getProperty("user.dir") +fileSeperator+ "TestReport";
-    static String timeStamp=new SimpleDateFormat("yyyy_MM_dd_hh_mm_a").format(new Date());
-    private static String reportFileLocation =  reportFilepath +fileSeperator+ reportFileName+timeStamp+".html";
-    public static String URL;
-    
-    @BeforeSuite
-    public static void setUp() throws Exception
-    {
-    	properties.load(new FileInputStream(new File("./src/test/resources/environment.properties")));	
-		//read excel file
-		String sExcelFilePath=System.getProperty("user.dir")+"\\data\\"+properties.getProperty("DataFile");
-		DataInputProvider.setExcelFile(sExcelFilePath,properties.getProperty("DriverSheetName"));
-    	URL=DataInputProvider.getCellData_ColName(1, "Environment",properties.getProperty("DriverSheetName"));
-    	String fileName = getReportPath(reportFilepath);
-    	htmlReporter = new ExtentHtmlReporter(fileName);
-        extent = new ExtentReports();
-        extent.attachReporter(htmlReporter);
-         
-        extent.setSystemInfo("OS", "Windows");
-        extent.setSystemInfo("Environment", URL);         
-        htmlReporter.config().setChartVisibilityOnOpen(true);
-        htmlReporter.config().setReportName("Biosimilar Calculator Automation Test Results");
-        htmlReporter.config().setTestViewChartLocation(ChartLocation.BOTTOM);
-        htmlReporter.config().setTheme(Theme.STANDARD);
-    }
-    
-   	public void beforeMethod() throws Exception {
-   	//	Runtime.getRuntime().exec("taskkill /F /IM chromedriver.exe /T");
-   		
-   		properties.load(new FileInputStream(new File("./src/test/resources/environment.properties")));
-        //System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\chromedriver.exe");
-   		WebDriverManager.chromedriver()//.cachePath("./src/test/resources/")
-   		.setup();
-   		ChromeOptions options = new ChromeOptions();  		
-   		if(properties.getProperty("Headless").equalsIgnoreCase("true"))
-   			options.setHeadless(true);
-   		webdriver = new ChromeDriver(options);
-   		driver = new EventFiringWebDriver(webdriver);
-   		driver.register(this);
 
-   		tlDriver.set(driver);		
-   		getDriver().manage().window().maximize();
-   		getDriver().get(URL);
-   		getDriver().manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-   		
-   	}
-   	
-    //Create the report path
-    private static String getReportPath (String path) {
-    	File testDirectory = new File(path);
-        if (!testDirectory.exists()) {
-        	if (testDirectory.mkdir()) {
-                System.out.println("Directory: " + path + " is created!" );
-                return reportFileLocation;
-            } else {
-                System.out.println("Failed to create directory: " + path);
-                return System.getProperty("user.dir");
-            }
-        } else {
-            System.out.println("Test results report directory already exists: " + path);
-        }
+	private static String reportFileName = "Test-Automation-Report";
+	private static String fileSeperator = System.getProperty("file.separator");
+	private static String reportFilepath = System.getProperty("user.dir") + fileSeperator + "TestReport";
+	static String timeStamp = new SimpleDateFormat("yyyy_MM_dd_hh_mm_a").format(new Date());
+	private static String reportFileLocation = reportFilepath + fileSeperator + reportFileName + timeStamp + ".html";
+	public static String URL;
+
+	@BeforeSuite
+	public static void setUp() throws Exception {
+		properties.load(new FileInputStream(new File("./src/test/resources/environment.properties")));
+		// read excel file
+		String sExcelFilePath = System.getProperty("user.dir") + "\\data\\" + properties.getProperty("DataFile");
+		DataInputProvider.setExcelFile(sExcelFilePath, properties.getProperty("DriverSheetName"));
+		URL = DataInputProvider.getCellData_ColName(1, "Environment", properties.getProperty("DriverSheetName"));
+		String fileName = getReportPath(reportFilepath);
+		htmlReporter = new ExtentHtmlReporter(fileName);
+		extent = new ExtentReports();
+		extent.attachReporter(htmlReporter);
+
+		extent.setSystemInfo("OS", "Windows");
+		extent.setSystemInfo("Environment", URL);
+		htmlReporter.config().setChartVisibilityOnOpen(true);
+		htmlReporter.config().setReportName("Biosimilar Calculator Automation Test Results");
+		htmlReporter.config().setTestViewChartLocation(ChartLocation.BOTTOM);
+		htmlReporter.config().setTheme(Theme.STANDARD);
+	}
+
+	public void beforeMethod() throws Exception {
+		// Runtime.getRuntime().exec("taskkill /F /IM chromedriver.exe /T");
+
+		properties.load(new FileInputStream(new File("./src/test/resources/environment.properties")));
+		// System.setProperty("webdriver.chrome.driver",
+		// "src\\test\\resources\\chromedriver.exe");
+		WebDriverManager.chromedriver()// .cachePath("./src/test/resources/")
+				.setup();
+		ChromeOptions options = new ChromeOptions();
+		if (properties.getProperty("Headless").equalsIgnoreCase("true"))
+			options.setHeadless(true);
+		webdriver = new ChromeDriver(options);
+		driver = new EventFiringWebDriver(webdriver);
+		driver.register(this);
+
+		tlDriver.set(driver);
+		getDriver().manage().window().maximize();
+		getDriver().get(URL);
+		getDriver().manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+	}
+
+	// Create the report path
+	private static String getReportPath(String path) {
+		File testDirectory = new File(path);
+		if (!testDirectory.exists()) {
+			if (testDirectory.mkdir()) {
+				System.out.println("Directory: " + path + " is created!");
+				return reportFileLocation;
+			} else {
+				System.out.println("Failed to create directory: " + path);
+				return System.getProperty("user.dir");
+			}
+		} else {
+			System.out.println("Test results report directory already exists: " + path);
+		}
 		return reportFileLocation;
-    }
-   	    
-  public void closeAllBrowsers()
-    {
-    	try {
+	}
+
+	public void closeAllBrowsers() {
+		try {
 			getDriver().close();
 			test.log(Status.PASS, "The opened browsers are closed");
 		} catch (Exception e) {
 			test.log(Status.FAIL, "Unexpected error occured in Browser");
 		}
-    }
-    @AfterSuite
-    public void tearDown()
-    {
-        extent.flush();
-    }
+	}
+
+	@AfterSuite
+	public void tearDown() {
+		extent.flush();
+	}
 }
--- BiosimilarCalculator/src/main/java/pages/LoginPage.java
+++ BiosimilarCalculator/src/main/java/pages/LoginPage.java
@@ -1,47 +1,25 @@
 package pages;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.Map;
-import java.util.StringTokenizer;
 
-import org.json.JSONObject;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Cookie;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import services.WebDriverServiceImpl;
-import utils.APIUtils;
-import utils.JSONUtils;
-import utils.SessionManager;
 
-public class LoginPage extends WebDriverServiceImpl{
+public class LoginPage extends WebDriverServiceImpl {
 
 	public LoginPage loginToApplication(String username, String password) throws InterruptedException {
-		/*type(getDriver().findElement(By.id("identifier")),username,"Username");
-		click(getDriver().findElement(By.xpath("//input[@value='Next']")),"Next");
+
+		type(getDriver().findElement(By.id("identifier")), username, "Username");
+		click(getDriver().findElement(By.xpath("//input[@value='Next']")), "Next");
 		Thread.sleep(3000);
-		type(getDriver().findElement(By.id("password")),password,"Password");
-		Thread.sleep(3000);
-		click(getDriver().findElement(By.xpath("//input[@value='Sign In']")),"Signin");	
-		Thread.sleep(5000);*/
-		type(getDriver().findElement(By.id("identifier")),username,"Username");
-		click(getDriver().findElement(By.xpath("//input[@value='Next']")),"Next");
-		Thread.sleep(3000);
-		type(getDriver().findElement(By.name("loginfmt")),username,"Username");
-		click(getDriver().findElement(By.xpath("//input[@type='submit']")),"Next");
+		type(getDriver().findElement(By.name("loginfmt")), username, "Username");
+		click(getDriver().findElement(By.xpath("//input[@type='submit']")), "Next");
 		Thread.sleep(3000);
 
 		try {
@@ -57,14 +35,14 @@ public class LoginPage extends WebDriverServiceImpl{
 		}
 		return this;
 	}
-	
+
 	@SuppressWarnings("deprecation")
 	public LoginPage loginToApp(String username, String password) throws InterruptedException, IOException {
-		type(getDriver().findElement(By.id("identifier")),username,"Username");
-		click(getDriver().findElement(By.xpath("//input[@value='Next']")),"Next");
+		type(getDriver().findElement(By.id("identifier")), username, "Username");
+		click(getDriver().findElement(By.xpath("//input[@value='Next']")), "Next");
 		Thread.sleep(3000);
-		type(getDriver().findElement(By.name("loginfmt")),username,"Username");
-		click(getDriver().findElement(By.xpath("//input[@type='submit']")),"Next");
+		type(getDriver().findElement(By.name("loginfmt")), username, "Username");
+		click(getDriver().findElement(By.xpath("//input[@type='submit']")), "Next");
 		Thread.sleep(3000);
 
 		try {
@@ -78,135 +56,124 @@ public class LoginPage extends WebDriverServiceImpl{
 			Thread.sleep(10000);
 			click(getDriver().findElement(By.id("idSIButton9")), "Sign in");
 		}
-		
-		// Create a file to store Login Information  - use it for 1st time
-		File file = new File("./src/test/resources/Cookiefile.data"); 
-		
-		//Store the cookie for 1st time 
-		try{ 
-		// Delete old file if already exists
-		file.delete(); 
-		file.createNewFile(); 
-		FileWriter filew = new FileWriter(file); 
-		BufferedWriter Bwritecookie = new BufferedWriter(filew); //Getting the cookie information 
-		for(Cookie ck : getDriver().manage().getCookies()) {
-		System.out.println("cookie details: " + "\nName: " + ck.getName() + "\nValue: " + ck.getValue()
-							+ "\nDomain: " + ck.getDomain() + "\nPath: " + ck.getPath() + "\nExpiry: " + ck.getExpiry()
-							+ "\nIsSecure: " + ck.isSecure());
-		Bwritecookie.write((ck.getName()+";"+ck.getValue()+";"+ck.getDomain()+";"+ck.getPath()+";"+ck.getExpiry()+";"+ck.isSecure())); 
-		Bwritecookie.newLine(); 
-		} 
-		Bwritecookie.close(); 
-		filew.close(); 
-		}
-		catch(Exception ex) 
-		{ 
-		ex.printStackTrace(); 
-		} 
-	
-		
-		//Read cookie information - use from 2nd time
-	/*	try{
-			//File file = new File("Cookie.data");
-			FileReader fileReader = new FileReader(file);
-			BufferedReader Buffreader = new BufferedReader(fileReader);
-			String strline;
-			while ((strline = Buffreader.readLine()) != null) {
-				StringTokenizer token = new StringTokenizer(strline, ";");
-				while (token.hasMoreTokens()) {
-					String name = token.nextToken();
-					String value = token.nextToken();
-					String domain = token.nextToken();
-					String path = token.nextToken();
-					Date expiry = null;
-					String val;
-					if (!(val = token.nextToken()).equals("null")) {
-						expiry = new Date(val);
-					}
-					@SuppressWarnings("removal")
-					Boolean isSecure = new Boolean(token.nextToken()).booleanValue();
-					Cookie ck = new Cookie(name, value, domain, path, expiry, isSecure);
-					System.out.println("cookie details: " + "\nName: " + ck.getName() + "\nValue: " + ck.getValue()
-							+ "\nDomain: " + ck.getDomain() + "\nPath: " + ck.getPath() + "\nExpiry: " + ck.getExpiry()
-							+ "\nIsSecure: " + ck.isSecure());
 
-					getDriver().manage().addCookie(ck);
-				}
-			} Buffreader.close();
-			}catch(Exception ex){
-			ex.printStackTrace();
+		// Create a file to store Login Information - use it for 1st time
+		File file = new File("./src/test/resources/Cookiefile.data");
+
+		// Store the cookie for 1st time
+		try {
+			// Delete old file if already exists
+			file.delete();
+			file.createNewFile();
+			FileWriter filew = new FileWriter(file);
+			BufferedWriter Bwritecookie = new BufferedWriter(filew); // Getting the cookie information
+			for (Cookie ck : getDriver().manage().getCookies()) {
+				System.out.println("cookie details: " + "\nName: " + ck.getName() + "\nValue: " + ck.getValue()
+						+ "\nDomain: " + ck.getDomain() + "\nPath: " + ck.getPath() + "\nExpiry: " + ck.getExpiry()
+						+ "\nIsSecure: " + ck.isSecure());
+				Bwritecookie.write((ck.getName() + ";" + ck.getValue() + ";" + ck.getDomain() + ";" + ck.getPath() + ";"
+						+ ck.getExpiry() + ";" + ck.isSecure()));
+				Bwritecookie.newLine();
 			}
-			getDriver().get("https://stage-acute-care-reimbursement-calculators.premierinc.com/calculators-dashboard");
-			//type(getDriver().findElement(By.id("identifier")),username,"Username");
-			//click(getDriver().findElement(By.xpath("//input[@value='Next']")),"Next");
-			Thread.sleep(3000);
-			
-	*/
-			
+			Bwritecookie.close();
+			filew.close();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+		// Read cookie information - use from 2nd time
+		/*
+		 * try{ //File file = new File("Cookie.data"); FileReader fileReader = new
+		 * FileReader(file); BufferedReader Buffreader = new BufferedReader(fileReader);
+		 * String strline; while ((strline = Buffreader.readLine()) != null) {
+		 * StringTokenizer token = new StringTokenizer(strline, ";"); while
+		 * (token.hasMoreTokens()) { String name = token.nextToken(); String value =
+		 * token.nextToken(); String domain = token.nextToken(); String path =
+		 * token.nextToken(); Date expiry = null; String val; if (!(val =
+		 * token.nextToken()).equals("null")) { expiry = new Date(val); }
+		 * 
+		 * @SuppressWarnings("removal") Boolean isSecure = new
+		 * Boolean(token.nextToken()).booleanValue(); Cookie ck = new Cookie(name,
+		 * value, domain, path, expiry, isSecure); System.out.println("cookie details: "
+		 * + "\nName: " + ck.getName() + "\nValue: " + ck.getValue() + "\nDomain: " +
+		 * ck.getDomain() + "\nPath: " + ck.getPath() + "\nExpiry: " + ck.getExpiry() +
+		 * "\nIsSecure: " + ck.isSecure());
+		 * 
+		 * getDriver().manage().addCookie(ck); } } Buffreader.close(); }catch(Exception
+		 * ex){ ex.printStackTrace(); } getDriver().get(
+		 * "https://stage-acute-care-reimbursement-calculators.premierinc.com/calculators-dashboard"
+		 * ); //type(getDriver().findElement(By.id("identifier")),username,"Username");
+		 * //click(getDriver().findElement(By.xpath("//input[@value='Next']")),"Next");
+		 * Thread.sleep(3000);
+		 * 
+		 */
+
 		/**
-   		 * test code to store session information
-   		 * 
-   		 */
-   	  // Get Cookies
+		 * test code to store session information
+		 * 
+		 */
+		// Get Cookies
 
-       //SessionManager sessionManager = new SessionManager(getDriver());
+		// SessionManager sessionManager = new SessionManager(getDriver());
 
-        // Method 1:
-       // sessionManager.storeSessionFile("biosimilar","Gomathipriya_nathan@premierinc.com");
+		// Method 1:
+		// sessionManager.storeSessionFile("biosimilar","Gomathipriya_nathan@premierinc.com");
 
-        //sessionManager.usePreviousLoggedInSession("biosimilar");
+		// sessionManager.usePreviousLoggedInSession("biosimilar");
 
-        // Method 2:
+		// Method 2:
 
-        /* 
-        // Completely bypass UI
+		/*
+		 * // Completely bypass UI
+		 * 
+		 * // POST - https://stage-acute-care-reimbursement-calculators.premierinc.com/
+		 * 
+		 * JSONObject payload = new JSONObject();
+		 * payload.put("email","Gomathipriya_nathan@premierinc.com");
+		 * payload.put("password","Welcome@0724"); String Uri =
+		 * "https://stage-acute-care-reimbursement-calculators.premierinc.com/calculators-dashboard";
+		 * 
+		 * Map<String, String> cookies = new APIUtils().postCallGetCookies(payload,Uri);
+		 * 
+		 * sessionManager.byPassLoginUsingCookies(cookies,
+		 * "https://stage-acute-care-reimbursement-calculators.premierinc.com/");
+		 */
 
-        //  POST - https://stage-acute-care-reimbursement-calculators.premierinc.com/
-
-        JSONObject payload = new JSONObject();
-        payload.put("email","Gomathipriya_nathan@premierinc.com");
-        payload.put("password","Welcome@0724");
-        String Uri = "https://stage-acute-care-reimbursement-calculators.premierinc.com/calculators-dashboard";
-
-        Map<String, String> cookies =  new APIUtils().postCallGetCookies(payload,Uri);
-
-        sessionManager.byPassLoginUsingCookies(cookies,"https://stage-acute-care-reimbursement-calculators.premierinc.com/");
- */
-        
 		return this;
 	}
-	
+
 	public LoginPage typeUsername(String username) throws InterruptedException {
-		type(getDriver().findElement(By.id("identifier")),username,"Username");
-		click(getDriver().findElement(By.xpath("//input[@value='Next']")),"Next");
+		type(getDriver().findElement(By.id("identifier")), username, "Username");
+		click(getDriver().findElement(By.xpath("//input[@value='Next']")), "Next");
 		Thread.sleep(3000);
 		return this;
 	}
-		
+
 	public LoginPage typePassword(String password) throws InterruptedException {
-		type(getDriver().findElement(By.id("password")),password,"Password");
+		type(getDriver().findElement(By.id("password")), password, "Password");
 		Thread.sleep(3000);
 		return this;
 	}
 
 	public LoginPage clickSignIn() throws InterruptedException {
-		click(getDriver().findElement(By.xpath("//input[@value='Sign In']")),"Signin");	
+		click(getDriver().findElement(By.xpath("//input[@value='Sign In']")), "Signin");
 		Thread.sleep(5000);
 		return this;
 	}
-	
+
 	public LoginPage closePopupOnDashboardPage() throws InterruptedException {
-		click(getDriver().findElement(By.xpath("(//div[@id='root']//button)[6]")),"ClosePopup");	
+		click(getDriver().findElement(By.xpath("(//div[@id='root']//button)[6]")), "ClosePopup");
 		Thread.sleep(5000);
 		return this;
 	}
-	
+
 	public GeneralDashboardPage clickClinicalResourcesApplication(String calculatorPage) throws InterruptedException {
-		click(getDriver().findElement(By.xpath("//span[contains(text(),'Applications')]")),"Applications");	
+		click(getDriver().findElement(By.xpath("//span[contains(text(),'Applications')]")), "Applications");
 		Thread.sleep(5000);
 		scrollDown(getDriver().findElement(By.xpath("//div[contains(text(),'Biosimilar Calculator')]")));
 		Thread.sleep(5000);
-		click(getDriver().findElement(By.xpath("//div[contains(text(),'Biosimilar Calculator')]")),"Biosimilar Calculator");
+		click(getDriver().findElement(By.xpath("//div[contains(text(),'Biosimilar Calculator')]")),
+				"Biosimilar Calculator");
 		Thread.sleep(10000);
 		ArrayList<String> tabs = new ArrayList<String>(getDriver().getWindowHandles());
 		getDriver().switchTo().window(tabs.get(1));
